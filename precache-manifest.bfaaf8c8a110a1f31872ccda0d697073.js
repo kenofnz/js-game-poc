a22d2ede3cd1091ee66096fa8115e3edee6f5bc1
kenofnz <ken_kwan_master@hotmail.com>
2020-09-12 16:29:09 +1200

    Updates

---
+++ precache-manifest.bfaaf8c8a110a1f31872ccda0d697073.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "191712acd663d17a8d746336188a2c55",
+    "url": "/kenofnz/js-game-poc/index.html"
+  },
+  {
+    "revision": "7dc259a671c4678962c6",
+    "url": "/kenofnz/js-game-poc/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e189d82d0588709662e2",
+    "url": "/kenofnz/js-game-poc/static/js/2.74168054.chunk.js"
+  },
+  {
+    "revision": "5ffe065bb4a7ca38dd922bcc709881ef",
+    "url": "/kenofnz/js-game-poc/static/js/2.74168054.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dc259a671c4678962c6",
+    "url": "/kenofnz/js-game-poc/static/js/main.7cd5ac75.chunk.js"
+  },
+  {
+    "revision": "cbeaedd65eb318ff4ec6",
+    "url": "/kenofnz/js-game-poc/static/js/runtime-main.eda3fff7.js"
+  }
+]);
